575e0d3e594b92b2bb3e1da67e18f0731966fe07
Mohamed Nabassoua <mnabassoua@gmail.com>
2018-01-08 15:40:15 -0500

    found the various functions by observing pattern of numbers from the outpu

--- Chapter1_2/exercise10.scm
+++ Chapter1_2/exercise10.scm
@@ -30,6 +30,8 @@
 ;;;for the function computed by the procedure g above given positive integers value of n
 ;;;We observe that as n increases by 1 unit, the value doubles from the previous one
 ;;;For example, when n=2, g=4 and increasing n by 1 unit gives n=3 and g=2*4=8
+;;;A clever observation would be to note that A(1,n) = 2^n. For example, g(3)=A(1,3)= 2^3 = 8
+;;;;Therefore g(n)=2^n
 
 
 ;(g 1)
@@ -52,16 +54,30 @@
 
 ;;;Trying a few values to identify what might the mathematical definition
 ;;;for the function computed by the procedure f above given positive integers value of n
-
+;;;Note that A(0,n)=2n. Therefore, f(n) = 2n
 
 (define (h n)
   (A 2 n))
 
 ;;;Trying a few values to identify what might the mathematical definition
 ;;;for the function computed by the procedure h above given positive integers value of n
+;;;Observe the following: 2=2^1; 4=2^2; 16=2^(2^2). 
+;;;Therefore h(n) = 2^(2^(2^..........) n times
+
+;(h 2)
+;Value: 4
+
+;(h 1)
+;Value: 2
+
+;(h 3)
+;Value: 16
+
+;(h 4)
+;Value: 65536
 
 (define (k n)
-  (* 5 5))
+  (* 5 n n))
 
-;;;Trying a few values to identify what might the mathematical definition
-;;;for the function computed by the procedure k above given positive integers value of n
+;;;Trivial case. k(n)=5n^2
+
