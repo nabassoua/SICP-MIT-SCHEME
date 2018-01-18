ab6b5feb30c9b63a1b88ea4bfe225ba944d71f12
Mohamed Nabassoua <mnabassoua@gmail.com>
2018-01-18 12:44:16 -0500

    no comments

---
+++ Chapter1_2/count-change.scm
@@ -0,0 +1,30 @@
+;;;;Procedure to compute the number of ways to change an amount a
+
+(define (count-change amount)
+  (cc amount 5))
+
+(define (cc amount kinds-of-coins)
+  (cond ((= amount 0) 1)
+	((or (< amount 0) (= kinds-of-coins 0)) 0)
+	(else (+ (cc amount
+		     (- kinds-of-coins 1))
+		 (cc (- amount
+			(first-denomination kinds-of-coins))
+		     kinds-of-coins)))))
+
+(define (first-denomination kinds-of-coins)
+  (cond ((= kinds-of-coins 1) 1)
+	((= kinds-of-coins 2) 5)
+        ((= kinds-of-coins 3) 10)
+        ((= kinds-of-coins 4) 25)
+        ((= kinds-of-coins 5) 50)))
+
+;;;Examples
+	
+;(count-change 100)
+;Value: 292
+
+;(count-change 11)
+;Value: 4
+
+;;;Order of growth of the space
