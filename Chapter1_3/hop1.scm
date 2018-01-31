e1620a0a5730c1f769bafeb393811afd1035f2ac
Mohamed Nabassoua <mnabassoua@gmail.com>
2018-01-31 18:40:35 -0500

    added additional procedures

--- Chapter1_3/hop1.scm
+++ Chapter1_3/hop1.scm
@@ -14,6 +14,22 @@
 (define (cubes x)
   (* x x x))
 
+(define (identity x)
+  x)
+
+(define (new-sum-ints a b)
+  (sum identity a inc b))
+
+
+(define (sum-integrals f a b dx)
+  (define (add-next x) (+ x dx))
+  (* dx (sum f (+ a (/ dx 2)) add-next b)))
+
+(define (pie-sum a b)
+  (define (add-next x) (+ x 4))
+  (define (term a) (/ 1.0 (* a (+ a 2))))
+  (sum term a add-next b))
+
 ;;;Test cases
 
 ;(sum-cubes 3 3)
@@ -21,3 +37,10 @@
 
 ;(sum-cubes 1 10)
 ;Value: 3025
+
+;Value: sum-integrals
+
+
+
+
+
