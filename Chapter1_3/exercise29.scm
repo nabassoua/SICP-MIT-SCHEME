ba283a68bf372cdc9ae17fd264554b321e2a8546
Mohamed Nabassoua <mnabassoua@gmail.com>
2018-01-31 18:41:16 -0500

    fixed the issue of the index k

--- Chapter1_3/exercise29.scm
+++ Chapter1_3/exercise29.scm
@@ -16,11 +16,29 @@
 	((= n k) b)
 	(else (+ k (/ (- b a) n)))))
 
-(define (simpson f a b n k)
-  (* (* (sum f a (next-point a b k n) b)
-     (coefficient k n))
-     (/ (- b a) 3)))
+(define (simpson f a b n )
+  (define h (/ (- b a) n))
+  (define (y-of-k k) (f (+ a (* k h))))
+  (define (increment x) (+ x 1)) 
+  (define (even? k)
+    (= (remainder k 2) 0))
+  (define (term k)
+    (* (cond ((= k 0) 1)
+	  ((= k n) 1)
+	  ((even? k) 2)
+	  (else 4)) (y-of-k k)))
+  (* (/ h 3)  (sum term 0 increment n)))
+		  
+			   
+;(simpson cubes 0 1 100.0)
+;Value: .24999999999999992
 
-;;;Test cases
+;(simpson cubes 0 1 1000.0)
+;Value: .2500000000000003			   
+		  
+     
+     
 
-(simpson cubes 0 1 100 1) 
+
+
+
