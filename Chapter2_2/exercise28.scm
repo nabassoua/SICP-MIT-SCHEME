fe76a6577e6bb7b6d81983896dd6913580ee9a69
Mohamed Nabassoua <mnabassoua@gmail.com>
2018-07-30 14:50:50 -0400

    now works

--- Chapter2_2/exercise28.scm
+++ Chapter2_2/exercise28.scm
@@ -5,8 +5,12 @@
       '()
       (let ((first (car mytree))
 	    (rest (cdr mytree)))
-	(append (list first)
-		(fringe rest)))))
+	(append 
+         ;;Always check if the car is a pair, if yes use recursion, else make it into a list
+	 (if (pair? first)
+	     (fringe first)
+	     (list first))
+	 (fringe rest)))))
 	    
 		   
 (define (append_list list1 list2)
@@ -16,5 +20,10 @@
 
 (define x (list (list 1 2) (list 3 4)))
 
+;;;Incorrect output
 ;(fringe x)
 ;Value 15: ((1 2) (3 4))
+
+;;;Correct output
+;(fringe x)
+;Value 14: (1 2 3 4)
