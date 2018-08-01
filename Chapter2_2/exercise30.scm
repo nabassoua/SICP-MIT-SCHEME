1395dc3bb7534e5e1071fedb74d0c34a5793e9d6
Mohamed Nabassoua <mnabassoua@gmail.com>
2018-08-01 09:45:42 -0400

    tree manipulation exercise..

---
+++ Chapter2_2/exercise30.scm
@@ -0,0 +1,14 @@
+;;;Squaring the elements of a tree
+
+(define (square-tree tree)
+  (cond ((null? tree) '())
+	((not (pair? tree)) (square tree))
+	(else
+	 (cons (square-tree (car tree))
+	       (square-tree (cdr tree))))))
+
+(square-tree (list 1 (list 2 (list 3 4) 5) (list 6 7)))
+;Error message
+;The object (6 7), passed as the first argument to integer-zero?, is not the correct type.
+;After fixing error we get the correct output
+;Value 13: (1 (4 (9 16) 25) (36 49))
