b43c45fe7851fac7996adac5934f44fd7bb3e123
Mohamed Nabassoua <mnabassoua@gmail.com>
2018-08-06 11:46:42 -0400

    tree map fixed by passing an element of the true instead of the the entire tree itself

--- Chapter2_2/exercise31.scm
+++ Chapter2_2/exercise31.scm
@@ -3,13 +3,15 @@
 (define (tree-map f tree)
   (map (lambda(x)
 	 (if (pair? x)
-	     (tree-map f tree)
+	     (tree-map f x)
 	     (f x)))
        tree))
 
 (define (square-tree tree)
   (tree-map square tree))
 
-;(square-tree (list 1 (list 2 (list 3 4) 5) (list 6 7)))
+(square-tree (list 1 (list 2 (list 3 4) 5) (list 6 7)))
 ;;First attempt returned the error message below. 
 ;Aborting!: maximum recursion depth exceeded
+;Correct solution. You should alsways pass an element of the tree and not the entire tree itself
+;Value 13: (1 (4 (9 16) 25) (36 49))
