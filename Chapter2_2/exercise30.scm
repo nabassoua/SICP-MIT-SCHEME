2d7504114a4370920bf04fd8c227e284883735d1
Mohamed Nabassoua <mnabassoua@gmail.com>
2018-08-06 11:19:54 -0400

    tree manipulation using map

--- Chapter2_2/exercise30.scm
+++ Chapter2_2/exercise30.scm
@@ -12,3 +12,14 @@
 ;The object (6 7), passed as the first argument to integer-zero?, is not the correct type.
 ;After fixing error we get the correct output
 ;Value 13: (1 (4 (9 16) 25) (36 49))
+
+(define (square-tree2 tree)
+  (map (lambda(x)
+	 (if (pair? x)
+	     (square-tree2 x)
+	     (square x)))
+       tree))
+
+;(square-tree2 (list 1 (list 2 (list 3 4) 5) (list 6 7)))
+;Value 15: (1 (4 (9 16) 25) (36 49))
+
