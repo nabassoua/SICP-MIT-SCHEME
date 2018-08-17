c5d1852ad0d8ceb9c7608ad443faa90da97ef43e
Mohamed Nabassoua <mnabassoua@gmail.com>
2018-08-17 08:57:00 -0400

    Horner's rule as accumulation...

---
+++ Chapter2_2/exercise34.scm
@@ -0,0 +1,13 @@
+;;;Horner's rule using accumulate
+
+(load "sequences.scm")
+
+(define (horner-eval x coefficient-sequence)
+  (accumulate (lambda (this-coeff higher-terms)
+		(+ (* higher-terms x)
+		   this-coeff))
+	      0
+	      coefficient-sequence))
+
+;(horner-eval 2 (list 1 3 0 5 0 1))
+;Value: 79
