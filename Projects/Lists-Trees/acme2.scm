fa02b31a936fc30dc2c5893f53292295c1481de5
Mohamed Nabassoua <mohamednabassoua@Mohameds-MacBook-Air.local>
2018-10-02 15:09:13 -0400

    started the working file

---
+++ Projects/Lists-Trees/acme2.scm
@@ -0,0 +1,40 @@
+;;;Solutions to the exercises in the attached problem set
+
+;;;Quick review: reproducing map,filter and accumulate from memory
+
+(define (my-map fct seq)
+  (if (null? seq)
+      '()
+      (cons (fct (car seq))
+	    (my-map fct (cdr seq)))))
+
+;(my-map square (list 1 2 3 4))
+;Value 14: (1 4 9 16)
+
+(define (my-filter predicate? seq)
+  (cond ((null? seq) '())
+	((predicate? (car seq))(cons (car seq) (my-filter predicate? (cdr seq))))
+	(else 
+	 (my-filter predicate? (cdr seq)))))
+
+;(my-filter even? (list 1 2 4 5 7))
+;Value 15: (2 4)
+      
+(define (my-accumulate op initial seq)
+  (if (null? seq)
+      initial
+      (op (car seq)
+	  (my-accumulate op initial (cdr seq)))))
+
+;(my-accumulate + 0 (list 1 2 3 5 7))
+;Value: 18
+
+;(my-accumulate * 1 (list 1 2 3 5 7))
+;Value: 210
+
+;(my-accumulate cons '() (list 1 2 3 5 7))
+;Value 18: (1 2 3 5 7)
+
+
+
+
