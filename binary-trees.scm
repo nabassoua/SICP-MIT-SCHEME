f0cf828db38e1d53b7b4b161dc2d86d2be9ba7d8
Mohamed Nabassoua <mohamednabassoua@Mohameds-MacBook-Air.local>
2019-02-27 12:19:29 -0500

    the tree example given is now correct....

--- binary-trees.scm
+++ binary-trees.scm
@@ -21,8 +21,20 @@
 	((> x (entry set))
 	 (element-of-set? x (right-branch set)))))
 
-(define simple-tree (make-tree 1
-			       (make-tree 2 1 5)
-			       (make-tree 5 3 6)))
+(define simple-tree (make-tree 2
+			       (make-tree 1 '() '())
+			       (make-tree 5
+					  (make-tree 3 '() '())
+					  (make-tree 6 '() '()))))
+
+;Value 14: (2 (1 () ()) (5 (3 () ()) (6 () ())))
+
+;(element-of-set? 3 simple-tree)
+;Value: #t
+
+;(element-of-set? 9 simple-tree)
+;Value: #f
+
+
 			       
 			       
