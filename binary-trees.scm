e58975f836b8c8b89da5cf800b47e3b520527d12
Mohamed Nabassoua <mohamednabassoua@Mohameds-MacBook-Air.local>
2019-02-27 11:46:04 -0500

    the tree example given is not correct....

---
+++ binary-trees.scm
@@ -0,0 +1,28 @@
+;;;Binary Tree
+
+
+(define (entry tree)
+  (car tree))
+
+(define (left-branch tree)
+  (cadr tree))
+
+(define (right-branch tree)
+  (caddr tree))
+
+(define (make-tree entry left right)
+  (list entry left right))
+
+(define (element-of-set? x set)
+  (cond ((null? set) false)
+	((= x (entry set)) true)
+	((< x (entry set))
+	 (element-of-set? x (left-branch set)))
+	((> x (entry set))
+	 (element-of-set? x (right-branch set)))))
+
+(define simple-tree (make-tree 1
+			       (make-tree 2 1 5)
+			       (make-tree 5 3 6)))
+			       
+			       
