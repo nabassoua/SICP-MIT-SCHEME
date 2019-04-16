28211379d6b734b7eb5aa2fad647635a887f97f2
Mohamed Nabassoua <mohamednabassoua@Mohameds-MacBook-Air.local>
2019-04-16 13:32:07 -0400

    exercise 17..

--- Projects/DB/ch2-key-exercises.scm
+++ Projects/DB/ch2-key-exercises.scm
@@ -105,3 +105,29 @@
       '()
       (cons (proc (car items))
 	    (my-map proc (cdr items)))))
+
+
+;;;Exercise 26
+
+(define x (list 1 2 3))
+(define y (list 4 5 6))
+
+;(append x y)
+;Value 14: (1 2 3 4 5 6)
+
+;(cons x y)
+;Value 15: ((1 2 3) 4 5 6)
+
+;(list x y)
+;Value 16: ((1 2 3) (4 5 6))
+
+;;;Exercise 17
+
+(define (last-pair lst)
+  (if (null? (cdr lst))
+      (list (car lst))
+      (last-pair (cdr lst))))
+
+;(last-pair (list 1 4 9 16 25))
+;Value 24: (25)
+
