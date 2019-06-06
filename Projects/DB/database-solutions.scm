c0c69a249176f2b189f3c36af1ad9dddd4e83df1
Mohamed Nabassoua <mohamednabassoua@Mohameds-MacBook-Air.local>
2019-06-06 15:27:06 -0400

    solutions to the lab exercises...

---
+++ Projects/DB/database-solutions.scm
@@ -0,0 +1,14 @@
+;;;Database manipulation
+
+(load "database.scm")
+
+;;;Lab exercise 1
+
+(define (average-age my-data)
+  TBD)
+
+(define (names-in-database my-data)
+  TBD)
+
+(define (names-of-people-over-65 my-data)
+  TBD)
