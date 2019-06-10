40c313dce2576674a74e45a817cf8c936ce36da5
Mohamed Nabassoua <mohamednabassoua@Mohameds-MacBook-Air.local>
2019-06-10 09:45:38 -0400

    computed average age in a sample database...

--- Projects/DB/database-solutions.scm
+++ Projects/DB/database-solutions.scm
@@ -1,11 +1,41 @@
 ;;;Database manipulation
 
 (load "database.scm")
+(load "ch2-key-exercises.scm")
 
 ;;;Lab exercise 1
 
-(define (average-age my-data)
-  TBD)
+;;collapse the sub-lists into 1 list
+
+(define single-list (fringe linear-test-data))
+   
+;;extract all ages from the collapsed list
+
+(define (age-list my-data)
+  (map cons
+       (filter integer? my-data)))
+
+(define (length lst)
+  (if (null? lst)
+      0
+      (+ 1 (length (cdr lst)))))
+				     
+
+(define (average-age db)
+  (/ (accumulate (lambda (x y)
+		   (+ (age (age-struct x)) y))
+		 0
+		 db)
+     (accumulate (lambda (x y)
+		   (+ 1.0 y))
+		 0
+		 db)))
+
+;(average-age linear-test-data)
+;Value: 60.76111111111111
+
+
+
 
 (define (names-in-database my-data)
   TBD)
